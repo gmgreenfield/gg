539fcbe2a5f909095242d1df8a0006d7efb4fdf2
Graham Greenfield <grahamg@gmail.com>
2026-09-09 02:15:17 -0500

    Delete characters from dynamic rows

--- editor.c
+++ editor.c
@@ -322,6 +322,35 @@ int insert_char(editor_state *s, int key) {
     return 0;
 }
 
+int delete_char(editor_state *s) {
+   if(s->cursor_y < 0 || (size_t)s->cursor_y >= s->file_row_count) {
+      return -1;
+   }
+
+   editor_row *row = &s->file_rows[s->cursor_y];
+
+   if(s->cursor_x == 0) {
+      return 0;
+   }
+
+   if (s->cursor_x < 0 || (size_t)s->cursor_x > row->length) {
+      return -1;
+   }
+
+   size_t position = (size_t)s->cursor_x;
+
+   memmove(
+       &row->chars[position-1],
+       &row->chars[position],
+       row->length - position + 1
+   );
+
+   row->length--;
+   s->cursor_x--;
+
+   return 0;
+}
+
 int main(int argc, char **argv) {
     editor_state p = {0};
     int key;
@@ -399,14 +428,11 @@ int main(int argc, char **argv) {
                 break;
             case 127:
             case CTRL_KEY('h'):
-                if(p.cursor_x > 0) { 
-                    memmove(
-                        &p.line[p.cursor_x - 1],
-                        &p.line[p.cursor_x],
-                        (size_t)(p.line_length - p.cursor_x)
-                    );
-                    p.cursor_x--;
-                    p.line_length--;
+                if(p.cursor_x > 0) {
+                    if(delete_char(&p) == -1) {
+                        fprintf(stderr, "failed to delete character.\n");
+                        return 1;
+                    };
                 }
                 break;
             default:
